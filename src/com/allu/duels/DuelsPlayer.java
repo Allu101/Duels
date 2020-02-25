1d44a04b3c423b826ebd9820a94a75747381906a
Allu101 <a.lahtinen101@gmail.com>
2020-02-25 12:32:20 +0200

    Refactor methods in alphabetical order

--- src/com/allu/duels/DuelsPlayer.java
+++ src/com/allu/duels/DuelsPlayer.java
@@ -7,71 +7,90 @@ public class DuelsPlayer {
 	private Player player;
 	private DuelsPlayer challengedPlayer;
 	private DuelsGame gameWhereJoined;
+	private PlayerSidebarHandler sidebarHandler;
 	
 	private int bestWinStreak;
 	private int currentWinStreak;
+	private int playedGames;
 	private int wins;
-	private int winLoseRatio;
 	
-	public DuelsPlayer(Player p) {
+	public DuelsPlayer(Player p, PlayerSidebarHandler sidebarHandler) {
 		this.player = p;
+		this.sidebarHandler = sidebarHandler;
+	}
+	
+	private void addCurrentWinStreak() {
+		currentWinStreak++;
+	}
+	
+	public void addWin() {
+		wins++;
+		addCurrentWinStreak();
+	}
+	
+	public int getBestWinStreak() {
+		return bestWinStreak;
+	}
+	
+	public DuelsPlayer getChallengedPlayer() {
+		return challengedPlayer;
+	}
+	
+	public int getCurrentWinStreak() {
+		return currentWinStreak;
+	}
+	
+	public DuelsGame getGameWhereJoined() {
+		return gameWhereJoined;
 	}
 
 	public Player getPlayer() {
 		return player;
 	}
 	
-	public boolean is(String uuid) {
-		return uuid.equals(player.getUniqueId().toString());
+	public PlayerSidebarHandler getSidebarHandler() {
+		return sidebarHandler;
 	}
-
-	public DuelsPlayer getChallengedPlayer() {
-		return challengedPlayer;
-	}
-
-	public void setChallengedPlayer(DuelsPlayer dp) {
-		this.challengedPlayer = dp;
-	}
-
-	public DuelsGame getGameWhereJoined() {
-		return gameWhereJoined;
-	}
-
-	public void setGameWhereJoined(DuelsGame gameWhereJoined) {
-		this.gameWhereJoined = gameWhereJoined;
-	}
-
 	
-	public int getBestWinStreak() {
-		return bestWinStreak;
+	public double getWinLoseRatio() {
+		int loses = playedGames - wins;
+		if(loses == 0) {
+			return wins;
+		}
+		return ((double)wins / loses);
 	}
-
-	public void setBestWinStreak(int bestWinStreak) {
-		this.bestWinStreak = bestWinStreak;
-	}
-
-	public int getCurrentWinStreak() {
-		return currentWinStreak;
-	}
-
-	public void setCurrentWinStreak(int currentWinStreak) {
-		this.currentWinStreak = currentWinStreak;
-	}
-
+	
 	public int getWins() {
 		return wins;
 	}
-
+	
+	public boolean is(String uuid) {
+		return uuid.equals(player.getUniqueId().toString());
+	}
+	
+	public void saveGameStats() {
+		playedGames++;
+		resetGameStats();
+	}
+	
+	public void setBestWinStreak(int bestWinStreak) {
+		this.bestWinStreak = bestWinStreak;
+	}
+	
+	public void setChallengedPlayer(DuelsPlayer dp) {
+		this.challengedPlayer = dp;
+	}
+	
+	public void setGameWhereJoined(DuelsGame gameWhereJoined) {
+		this.gameWhereJoined = gameWhereJoined;
+	}
+	
 	public void setWins(int wins) {
 		this.wins = wins;
 	}
-
-	public int getWinLoseRatio() {
-		return winLoseRatio;
-	}
-
-	public void setWinLoseRatio(int winLoseRatio) {
-		this.winLoseRatio = winLoseRatio;
+	
+	private void resetGameStats() {
+		sidebarHandler.resetGameSidebar();
 	}
 	
 }
