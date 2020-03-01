1b32394a38b481d306297481361e068ba210779d
Allu101 <a.lahtinen101@gmail.com>
2020-03-01 16:05:20 +0200

    Added name row to kit config

--- src/com/allu/duels/DuelsGame.java
+++ src/com/allu/duels/DuelsGame.java
@@ -10,7 +10,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+
 import com.allu.duels.utils.Gamemode;
 import com.allu.duels.utils.Kit;
 import com.allu.minigameapi.CountDownTimer;
